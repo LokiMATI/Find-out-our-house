33ebb3c0fa6bded7c37fb7249e9bb01bfe90cee8
Lucky <r.matigorov@yandex.ru>
2025-06-21 16:50:55 +0300

    Добавлена миграция Place

---
+++ FindOutOurHouse.DAL/Migrations/20250621131841_Added_Place.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindOutOurHouse.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Place : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Places",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Images = table.Column<List<Guid>>(type: "uuid[]", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Places", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Places");
+        }
+    }
+}

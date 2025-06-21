95bb9e46c8d67e8d7cecc34f0f13e8169ecf6589
Lucky <r.matigorov@yandex.ru>
2025-06-21 16:46:39 +0300

    Настроен Program.cs

--- FindOutOurHouse.API/Program.cs
+++ FindOutOurHouse.API/Program.cs
@@ -1,14 +1,29 @@
+using FindOutOurHouse.API.AppServices;
+using FindOutOurHouse.DAL;
+using FindOutOurHouse.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<PlaceService>();
+builder.Services.AddTransient<IPlaceRepository, PlaceRepository>();
+builder.Services.AddDbContext<EfDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger();   
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.Run();
